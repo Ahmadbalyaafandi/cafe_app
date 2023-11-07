f22796c2a55e6738a43dc88c88c91c599289e25a
Ahmadbalyaafandi <sayaafandi23@gmail.com>
2023-11-07 10:51:22 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/balya/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.balya
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/core.dart
@@ -0,0 +1,9 @@
+/*
+We believe, the class name must be unique. 
+If there is a conflicting class name in this file,
+it means you have to rename it to something more unique.
+*/
+export 'package:balya/module/dashboard/controller/dashboard_controller.dart';
+export 'package:balya/module/dashboard/view/dashboard_view.dart';
+export 'package:balya/module/mainmenu/controller/mainmenu_controller.dart';
+export 'package:balya/module/mainmenu/view/mainmenu_view.dart';
